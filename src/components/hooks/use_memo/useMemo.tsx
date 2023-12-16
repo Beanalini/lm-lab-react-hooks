9ee962262873d6256a3f3c7a310c438520c3d1e5
Beanalini <dat826@gmail.com>
2023-12-16 13:40:28 +0000

    refactor:useMemo -  changed from a hard coded value to allow user to enter a number on click

--- src/components/hooks/use_memo/useMemo.tsx
+++ src/components/hooks/use_memo/useMemo.tsx
@@ -1,30 +1,39 @@
-import { useState } from 'react';
-
+import { useState, useMemo } from "react";
 export const MemoExample = () => {
+  console.log("Rendering component...");
+  const [numberObj, setNumberObj] = useState({ input: 0 });
 
-	console.log("Rendering component...")
+  //Use state for managing user input
+  const [userInput, setUserInput] = useState("");
 
-	const [numberObj, setNumberObj] = useState({ input: 0 });
-	
-	const doubleNumber = slowFunction(numberObj.input)
+  const doubleNumber = useMemo(
+    () => slowFunction(numberObj.input),
+    [numberObj.input]
+  );
 
-	return (
-		<>
-			<h2>useMemo</h2>
-
-			<button onClick={() => setNumberObj({ input: 3 })}>Double 3</button>
-
-			<p className='use-memo__text'>{doubleNumber}</p>
-		</>
-	);
+  const handleButtonClick = () => {
+    const update = Number(userInput);
+    setNumberObj({ input: update });
+  };
+  return (
+    <>
+      <h2>useMemo</h2>
+      <input
+        value={userInput}
+        type="text"
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button onClick={handleButtonClick}>Double Number</button>
+      <p className="use-memo__text">{doubleNumber}</p>
+    </>
+  );
 };
-
 function slowFunction(num: number) {
-	console.log('calling slow function... 🐌');
-	for (let i = 0; i <= 1000000000; i++) {
-		// ⏰
-	}
-	const result = num * 2;
-	console.log(`result: ${result}`);
-	return result;
+  console.log("calling slow function... 🐌");
+  for (let i = 0; i <= 1000000000; i++) {
+    // ⏰
+  }
+  const result = num * 2;
+  console.log(`result: ${result}`);
+  return result;
 }
